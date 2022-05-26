251ef7f8e1e7c7925e118b22e9e5d5477f854a91
wutianming <2300281344@qq.com>
2022-05-26 18:38:51 +0800

    add semaphore to avoid busy waiting on the broadcaster side

--- lab3/2.cpp
+++ lab3/2.cpp
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 using namespace std;
 
@@ -16,19 +17,20 @@ const int MAXN = 32;
 const char header[] = "Message: ";
 
 struct msg {
-    char *data;
+    const char *data;
     size_t len;
     int counter;
     msg(char *data, size_t len) : data(data), len(len), counter(0) {}
-    // ~msg() {
-    //     delete data;
-    // }
+    ~msg() {
+        delete[] data;
+    }
 };
 
 // shared pointers provide thread-safe use_count counter
 // pthread_cond_t producer_ready[MAXN];
 queue<msg *> msg_queue[MAXN];
 pthread_mutex_t queue_mutexes[MAXN];
+sem_t msg_ready[MAXN];
 pthread_t workers[MAXN][2];
 bool online[MAXN];
 int fd_client[MAXN];
@@ -47,49 +49,52 @@ void *receive_msg(void *__id) {
     char *recvbuffer = new char[BUFLEN];
     list<msg *> buf_list;
     while ((len = recv(fd_client[id], recvbuffer, 1000, 0)) > 0) {
-        msg M(recvbuffer, len);
-        buf_list.push_back(&M);
+        msg *M = new msg(recvbuffer, len);
+        buf_list.push_back(M);
         for (int i = 0; i < MAXN; i++)
             if (online[i] && i != id) {
                 pthread_mutex_lock(&queue_mutexes[i]);
-                ++M.counter;
-                msg_queue[i].push(&M);
+                ++M->counter;
+                msg_queue[i].push(M);
                 pthread_mutex_unlock(&queue_mutexes[i]);
+                sem_post(&msg_ready[i]);
             }
         recvbuffer = new char[BUFLEN];
-        while (!buf_list.empty() && (**(buf_list.begin())).counter == 0) {
-            delete[] (*(buf_list.begin()));
+        while (!buf_list.empty() && (*(buf_list.begin()))->counter == 0) {
+            msg *M = *(buf_list.begin());
+            delete M;
             buf_list.pop_front();
         }
     }
     // getting 0 from recv() means client disconnect
     online[id] = false;
+    sem_post(&msg_ready[id]);   // releases the semaphore so that the broadcaster exits
     return NULL;
 }
 
 void *broadcast_msg(void *__id) {
     unsigned long long id = (unsigned long long)__id;
-    while (online[id]) {
-        // pthread_cond_wait(&producer_ready[id]);
-        while (!msg_queue[id].empty()) {
-            msg *M = msg_queue[id].front();
-            size_t prev = 0;
-            for (size_t idx = 0; idx < M->len; ++idx) {
-                if (M->data[idx] == '\n') {
-                    my_bulk_send(fd_client[id], header, sizeof(header) - 1, 0);
-                    my_bulk_send(fd_client[id], M->data + prev, idx - prev + 1, 0);
-                    prev = idx + 1;
-                }
-            }
-            if (prev != M->len) {
+    while (true) {
+        sem_wait(&msg_ready[id]);
+        if (!online[id]) break;
+        if (msg_queue[id].empty()) continue;
+        msg *M = msg_queue[id].front();
+        size_t prev = 0;
+        for (size_t idx = 0; idx < M->len; ++idx) {
+            if (M->data[idx] == '\n') {
                 my_bulk_send(fd_client[id], header, sizeof(header) - 1, 0);
-                my_bulk_send(fd_client[id], M->data + prev, M->len - prev, 0);
+                my_bulk_send(fd_client[id], M->data + prev, idx - prev + 1, 0);
+                prev = idx + 1;
             }
-            pthread_mutex_lock(&queue_mutexes[id]);
-            msg_queue[id].pop();
-            --M->counter;
-            pthread_mutex_unlock(&queue_mutexes[id]);
         }
+        if (prev != M->len) {
+            my_bulk_send(fd_client[id], header, sizeof(header) - 1, 0);
+            my_bulk_send(fd_client[id], M->data + prev, M->len - prev, 0);
+        }
+        pthread_mutex_lock(&queue_mutexes[id]);
+        msg_queue[id].pop();
+        --M->counter;
+        pthread_mutex_unlock(&queue_mutexes[id]);
     }
     while (!msg_queue[id].empty()) {
         pthread_mutex_lock(&queue_mutexes[id]);
@@ -124,6 +129,8 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < MAXN; i++)
         pthread_mutex_init(&queue_mutexes[i], NULL);
+    for (int i = 0; i < MAXN; i++)
+        sem_init(&msg_ready[i], 0, 0);
 
     while (true) {
         // accept new connections and launch new threads
